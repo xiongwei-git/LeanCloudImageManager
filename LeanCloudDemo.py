adffa5ac461eb1b47abf95e0c4ce030dd03f38cf
Ted <ted@lifang.com>
2015-07-03 18:52:59 +0800

    add function

--- LeanCloudDemo.py
+++ LeanCloudDemo.py
@@ -15,8 +15,9 @@ from leancloud import Query
 className = 'AllPictures'
 saveClassName = 'PythonTestData'
 allPictures = Object.extend(className)
-savePath = 'D://image//1-499//photo//'
+savePath = 'D://image//1-499//photo'
 allPictureUrlFile = '1-499.txt'
+errorPictureUrlFile = '1_499_error.txt'
 
 
 #--------Method------------#
@@ -45,6 +46,15 @@ def queryOneDataByPosition(position):
     if len(pictures) > 0:
       return pictures[0].get('image_src')
     return ""
+
+def getOnePictureByPosition(position):
+    query = Query(allPictures)
+    query.equal_to('position', position)
+    pictures = query.find()
+    if len(pictures) > 0:
+      return pictures[0]
+    else:
+      return None
 
 #download#
 def gDownloadWithFilename(url):
@@ -89,8 +99,77 @@ def gGetFileName(url):
     # if name.endswith("JPEG"):return name.replace("JPEG","jpg")
     # return name+".jpg"
 
+def checkFileIsExit(position):
+  imageurl = queryOneDataByPosition(position)
+  print imageurl
+  fileName = gGetFileName(imageurl)
+  checkFile = savePath+fileName
+  if os.path.exists(checkFile):
+      message = 'OK, the  file "%s" exists.'
+  else:
+      message = 'Sorry, the  file "%s"  not exists.'
+  print message % fileName
+
+def checkFileIsExitAndRem(position):
+  imageurl = queryOneDataByPosition(position)
+  print imageurl
+  fileName = gGetFileName(imageurl)
+  checkFile = savePath+fileName
+  if os.path.exists(checkFile):
+      return ''
+  else:
+      errorFileInfo = str(num)+'--'+fileName
+      return errorFileInfo
+  
 
 
+def changeServerFileName(position,newName):
+  query = Query(allPictures)
+  query.equal_to('position', position)
+  pictures = query.find()
+  pictureSelect =  pictures[0]
+  pictureSelect.set('image_src', newName)
+  pictureSelect.save()
+
+
+'''
+获得文件名称之前的url
+'''
+def getPreName(allUrl):
+  index = allUrl.rfind('/')
+  if index > 0:
+    return allUrl[0:index+1]
+  else:
+    return ''
+
+'''
+获取文件类型后缀
+'''
+def getFileType(fileName):
+  index = fileName.rfind('.')
+  if index > 0:
+    return fileName[index:len(fileName)]
+  else:
+    return '.jpg'
+
+
+def getNewNameByPosition(position,oldName):
+  prefix = '000_'
+  if position > 99:
+    prefix = str(position/100*100)+'_'
+  return prefix+'20150703_00'+ str(position) + getFileType(oldName)
+
+def renameLocalFileName(oldName,newName):
+  oldFile = savePath +'//' + oldName
+  if os.path.exists(oldFile):
+    os.rename(os.path.join(savePath,oldName),os.path.join(savePath,newName))
+    print 'the  file "%s" rename OK.' % oldName
+    return True
+  else:
+    print 'the  file "%s" not exists' % oldName
+    return False
+
+  
 
 #--------Main------------#
 if __name__ == '__main__':
@@ -99,6 +178,67 @@ if __name__ == '__main__':
       **	 Created on  2015-06-25	  **
       **	   @author: Ted		   **
       *************************************''')
+
+  '''
+  修改文件名称
+  '''
+  query = Query(allPictures)
+  for num in range(1,500):
+      query.equal_to('position', num)
+      pictures = query.find()
+      if len(pictures) > 0:
+        pictureSelect = pictures[0]
+        imageurl = pictureSelect.get('image_src')
+        if imageurl.find('00'+str(num)) > 0:
+          print ''
+        else:
+            print imageurl
+        # oldName = gGetFileName(imageurl)
+        # newFileName = getNewNameByPosition(num,oldName)
+        # newFileUrl = getPreName(imageurl) + newFileName
+        # print newFileUrl
+        # if renameLocalFileName(oldName,newFileName):
+        #     pictureSelect.set('image_src', newFileUrl)
+        #     pictureSelect.save()
+        # else:
+        #     print 'the server file change faild'
+        
+
+
+
+
+
+  # urlTest = queryOneDataByPosition(1)
+  # print getPreName(urlTest)
+
+  #renameLocalFileName('Test.jpg','Test1.jpg')
+
+
+#////////////////////////////////////////////////#
+  # errorFile=open(savePath + errorPictureUrlFile,'w+')
+  # for num in range(1,500):
+  #     noFileInfo = checkFileIsExitAndRem(num)
+  #     if len(noFileInfo) > 0:
+  #         errorFile.write(errorFileInfo)
+  #         errorFile.write("\n")
+  #     else:
+  #         message = 'OK, the  file "%s" exists.'
+  #         print message % str(num)
+  # errorFile.close()
+#////////////////////////////////////////////////#
+
+  #checkFileIsExit(1)
+
+  #changeServerFileName(290,'https://ununsplash.imgix.net/31/Traunsee-Toscana_2014-02-01.jpg')
+
+
+
+
+
+
+
+
+
   #queryOneData('556ea641e4b005426cff3032')
   #queryOneDataByPosition(1)
 # file=open(savePath + allPictureUrlFile,'w+')
@@ -115,16 +255,27 @@ if __name__ == '__main__':
 #     print(str(num)+" image url is null~~~~~~~~~~~~")
 # file.close()
 
-for num in range(521,522):
-  imageurl = queryOneDataByPosition(num)
-  print("get url = "+ imageurl)
-  # fileName = gGetFileName(imageurl)
-  # checkFile = savePath+fileName
-  # if os.path.exists(checkFile):
-  #       message = 'OK, the  file "%s" exists.'
-  # else:
-  #       message = 'Sorry, I cannot find the "%s" file.'
-  # print message % fileName
+
+# errorFile=open(savePath + errorPictureUrlFile,'w+')
+# for num in range(69,70):
+#   imageurl = queryOneDataByPosition(num)
+#   #print("get url = "+ imageurl)
+#   fileName = gGetFileName(imageurl)
+#   checkFile = savePath+fileName
+#   if os.path.exists(checkFile):
+#       message = 'OK, the  file "%s" exists.'
+#       print message % fileName
+#   else:
+#       errorFileInfo = str(num)+'--'+fileName
+#       errorFile.write(errorFileInfo)
+#       errorFile.write("\n")
+#       print errorFileInfo
+# errorFile.close()
+
+
+
+
+
 
 # query = Query(allPictures)
 # query.equal_to('position', 521)
